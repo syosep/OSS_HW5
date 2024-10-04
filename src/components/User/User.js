8b3b12c958388f7301097d00dfb228bc424775c9
심요셉 <s.yosep@kakao.com>
2024-10-04 20:33:15 +0900

    component

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -2,52 +2,62 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./User.css";
+
 const EditUser = () => {
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState({}); // Initialize as an object
+  const [loading, setLoading] = useState(true); // Loading state
   const { id } = useParams();
   const getUserApi = "http://localhost:3000/user";
 
-  useEffect(() => {
-    getUser();
-  }, []);
-
   const getUser = () => {
+    setLoading(true); // Start loading
     axios
-      .get(getUserApi.concat("/") + id)
-      .then((item) => {
-        setUser(item.data);
+      .get(`${getUserApi}/${id}`) // Simplified URL concatenation
+      .then((response) => {
+        setUser(response.data);
       })
       .catch((err) => {
-        console.log(err);
+        console.error(err); // Improved error logging
+      })
+      .finally(() => {
+        setLoading(false); // End loading
       });
   };
 
+  useEffect(() => {
+    getUser();
+  }, [id]); // Added id to dependency array
+
   return (
     <div className="user mt-5">
-      <table className="table table-bordered">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Name</td>
-        <td>{user.name}</td>
-      </tr>
-      <tr>
-        <td>Email</td>
-        <td>{user.email}</td>
-      </tr>
-      <tr>
-        <td>Phone</td>
-        <td>{user.phone}</td>
-      </tr>
-    </tbody>
-  </table>
+      {loading ? ( // Conditional rendering based on loading state
+        <p>Loading...</p>
+      ) : (
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Field</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>{user.name || 'N/A'}</td> {/* Provide fallback for empty values */}
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>{user.email || 'N/A'}</td>
+            </tr>
+            <tr>
+              <td>Phone</td>
+              <td>{user.phone || 'N/A'}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
+
 export default EditUser;
