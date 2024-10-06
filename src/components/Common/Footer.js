dd9a7ab96cfeac9ae91ac6a972d14b3e69924172
심요셉 <s.yosep@kakao.com>
2024-10-06 13:48:21 +0900

    footer css

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -3,7 +3,10 @@ import './Common.css';
 export default function Footer() {
   return (
     <div className='footer'>
-        Footer
+      <div className="footer-content">
+        <p>&copy; 2024 22100423 Shim, Yosep. All Rights Reserved.</p>
+        <p>Developed by Shim, Yosep | Contact: 22100423@handong.ac.kr</p>
+      </div>
     </div>
-  )
+  );
 }
