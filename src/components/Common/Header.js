a74562b510595bd6e8c736ffff076d05d8018998
심요셉 <s.yosep@kakao.com>
2024-10-06 13:05:10 +0900

    add header css

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -15,45 +15,47 @@ export default function Header({ isLoggedIn, setIsLoggedIn }) {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-        <div className="container-fluid">
-          <Link to="/" className="navbar-brand" href="#">
-            <span className="navbar-text">22100423 YosepShim</span>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3 shadow">
+        <div className="container">
+          <Link to="/" className="navbar-brand d-flex align-items-center" href="#">
+            <span className="navbar-text fw-bold fs-4">22100423 YosepShim</span>
           </Link>
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#mynavbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="mynavbar">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item mx-2">
+                <Link className="nav-link active fw-semibold" to="/">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/create-user">
+                <Link className="nav-link fw-semibold" to="/create-user">
                   Create User
                 </Link>
               </li>
               {isLoggedIn && (
-                <li className="nav-item">
-                  <Link className="nav-link" to="/show-user">
+                <li className="nav-item mx-2">
+                  <Link className="nav-link fw-semibold" to="/show-user">
                     Show User
                   </Link>
                 </li>
               )}
-              <li className="nav-item">
+              <li className="nav-item mx-2">
                 {isLoggedIn ? (
-                  <button className="nav-link btn" onClick={handleLogout}>
+                  <button className="btn btn-outline-light fw-semibold px-4" onClick={handleLogout}>
                     Logout
                   </button>
                 ) : (
-                  <Link className="nav-link" to="/login">
+                  <Link className="btn btn-light fw-semibold px-4" to="/login">
                     Login
                   </Link>
                 )}
