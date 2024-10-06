af650c1689d48cb37fcb0d576ad4da5a08506114
심요셉 <s.yosep@kakao.com>
2024-10-06 12:56:29 +0900

    activate during login

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -18,7 +18,7 @@ export default function Header({ isLoggedIn, setIsLoggedIn }) {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
         <div className="container-fluid">
           <Link to="/" className="navbar-brand" href="#">
-            <span className="navbar-text">React CRUD</span>
+            <span className="navbar-text">22100423 YosepShim</span>
           </Link>
           <button
             className="navbar-toggler"
@@ -40,11 +40,13 @@ export default function Header({ isLoggedIn, setIsLoggedIn }) {
                   Create User
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/show-user">
-                  Show User
-                </Link>
-              </li>
+              {isLoggedIn && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/show-user">
+                    Show User
+                  </Link>
+                </li>
+              )}
               <li className="nav-item">
                 {isLoggedIn ? (
                   <button className="nav-link btn" onClick={handleLogout}>
